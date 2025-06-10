83a18c7e7186409bbddc1e4ae75af3dc94c8efbc
Gomina Aksinya <a.e.gomina@gmail.com>
2025-06-10 16:31:18 +0300

    Commit 4 Тест  test_new_bun разделен на два теста: проверяющий нзвание новой булки и проверяющий её стоимость.

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -2,8 +2,13 @@ from praktikum.bun import Bun
 
 class TestBun:
 
-    # Тест на успешное создание новой булки
-    def test_new_bun(self):
+    # Тест на успешное создание новой булки с заданным названием
+    def test_new_bun_name_bun(self):
         bun = Bun("Булка смерти", 405)
         assert bun.get_name() == "Булка смерти"
+
+
+    # Тест на успешное создание новой булки с заданной стоимостью
+    def test_new_bun_price(self):
+        bun = Bun("Булка смерти", 405)
         assert bun.get_price() == 405
