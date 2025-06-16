114e8dd705c3034c6a9be98209432969f29c53f5
Gomina Aksinya <a.e.gomina@gmail.com>
2025-06-16 18:13:26 +0300

    Commit 6 Файл conflicts отчищен. Вся неиспользуемая информация удалена.

--- conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from praktikum.bun import Bun
-from praktikum.burger import Burger
-from praktikum.ingredient import Ingredient
-from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
-from praktikum.database import Database
-
